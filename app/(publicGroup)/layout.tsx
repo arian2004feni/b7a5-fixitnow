d99320beb5bf96f13175cedd2f02a74d8c195504
arian2004 <arian2004feni@gmail.com>
2026-09-16 19:11:24 +0600

    navbar ui modification with user menu items

--- app/(publicGroup)/layout.tsx
+++ app/(publicGroup)/layout.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/shared/Navbar";
 import Footer from "@/components/shared/Footer";
+import { getMe } from "@/services/getMe";
 
-export default function PublicLayout({ children }: LayoutProps<"/">) {
+export default async function PublicLayout({ children }: LayoutProps<"/">) {
+  const user = await getMe();
   return (
     <>
-      <Navbar />
+      <Navbar user={user} />
       <main className="flex-1">{children}</main>
       <Footer />
     </>
--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,12 +1,33 @@
 "use client";
 
-import { Menu, Wrench, X } from "lucide-react";
+import {
+  LogOutIcon,
+  Menu,
+  SettingsIcon,
+  UserIcon,
+  X,
+} from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
 import { Button } from "../ui/button";
+import { ApiResponse } from "@/types/api";
+import { User as UserResponse } from "@/types/user";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import Logo from "./Logo";
+import { toast } from "sonner";
 
-export default function Navbar() {
+export default function Navbar({ user }: { user: ApiResponse<UserResponse> }) {
   const [open, setOpen] = useState(false);
+
   const menuItems = [
     {
       name: "Find a Service",
@@ -21,17 +42,22 @@ export default function Navbar() {
       url: "/#how-it-works",
     },
   ];
+
+  const userMenuItems = [
+    { label: "Profile", icon: UserIcon, action: "profile" },
+    { label: "Settings", icon: SettingsIcon, action: "settings" },
+  ];
+
+  const handleUserMenuAction = async (action: string) => {
+    if (action === "logout") {
+      toast.success("succesfully triggred");
+    }
+  };
+
   return (
     <header className="border-b border-slate-200 bg-white/95 backdrop-blur sticky top-0 z-40">
       <div className="mx-auto flex h-18 max-w-7xl items-center justify-between px-5 lg:px-8">
-        <Link href="/" className="flex items-center gap-2.5">
-          <span className="grid size-9 place-items-center rounded-xl bg-blue-600 text-white shadow-sm">
-            <Wrench className="size-5" />
-          </span>
-          <span className="text-xl font-bold tracking-tight text-slate-950">
-            FixIt<span className="text-blue-600">Now</span>
-          </span>
-        </Link>
+        <Logo />
         <nav className="hidden items-center gap-8 md:flex">
           {menuItems.map((item) => (
             <Link
@@ -43,18 +69,70 @@ export default function Navbar() {
             </Link>
           ))}
         </nav>
-        <div className="hidden items-center gap-3 md:flex">
-          <Link href="/login">
-            <Button variant="ghost" className="text-slate-600">
-              Sign in
-            </Button>
-          </Link>
-          <Link href={"/register"}>
-            <Button className="rounded-lg bg-blue-600 hover:bg-blue-700">
-              Become a Technician
-            </Button>
-          </Link>
-        </div>
+        {user.success ? (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" size="icon" className="rounded-full">
+                <Avatar>
+                  <AvatarImage src="" alt="null" />
+                  <AvatarFallback>
+                    {user.data.name.split(" ")[0].slice(0, 2).toUpperCase()}
+                  </AvatarFallback>
+                </Avatar>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              align="end"
+              className="w-fit max-w-56 min-w-40"
+            >
+              <DropdownMenuLabel className="flex gap-2">
+                <Avatar>
+                  <AvatarImage src="" alt="null" />
+                  <AvatarFallback>
+                    {user.data.name.split(" ")[0].slice(0, 2).toUpperCase()}
+                  </AvatarFallback>
+                </Avatar>
+                <div>
+                  <p>{user.data.name}</p>
+                  <p className="line-clamp-1">{user.data.email}</p>
+                </div>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                {userMenuItems.map((item) => (
+                  <DropdownMenuItem
+                    key={item.label}
+                    onClick={() => handleUserMenuAction(item.action)}
+                  >
+                    <item.icon />
+                    {item.label}
+                  </DropdownMenuItem>
+                ))}
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                variant="destructive"
+                onClick={() => handleUserMenuAction("logout")}
+              >
+                <LogOutIcon />
+                Log out
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        ) : (
+          <div className="hidden items-center gap-3 md:flex">
+            <Link href="/login">
+              <Button variant="ghost" className="text-slate-600">
+                Sign in
+              </Button>
+            </Link>
+            <Link href={"/register"}>
+              <Button className="rounded-lg bg-blue-600 hover:bg-blue-700">
+                Become a Technician
+              </Button>
+            </Link>
+          </div>
+        )}
         <button
           aria-label="Open menu"
           className="md:hidden"
