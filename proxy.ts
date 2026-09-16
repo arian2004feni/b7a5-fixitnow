3c34c4b6b4477aa81801a82231af9baae441cce3
arian2004 <arian2004feni@gmail.com>
2026-09-16 21:56:19 +0600

    authorizing role based protected routes in proxy

--- proxy.ts
+++ proxy.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { Role } from "./types/user";
+import { jwtUtils } from "./utils/jwt";
 
 const AUTH_ROUTES = ["/login", "/register"];
 const PUBLIC_ROUTES = ["/", "/services", "/technicians"];
@@ -16,14 +17,20 @@ export async function proxy(request: NextRequest) {
   const accessToken = request.cookies.get("accessToken")?.value;
 
   const decodedToken = accessToken
-    ? (jwt.decode(accessToken) as JwtPayload)
+    ? jwtUtils.verifyToken(accessToken, process.env.JWT_ACCESS_SECRET!)
     : null;
 
   let userRole = null;
 
-  if (decodedToken) {
-    userRole = decodedToken.role;
+  if (!decodedToken?.success) {
+    request.cookies.delete("accessToken");
+    return NextResponse.redirect(new URL("/login", request.url));
   }
+
+  if (decodedToken.success && decodedToken.data) {
+    userRole = (decodedToken.data as JwtPayload).role;
+  }
+
   if (accessToken && AUTH_ROUTES.includes(pathname)) {
     if (userRole === Role.CUSTOMER) {
       return NextResponse.redirect(new URL("/customer-dashboard", request.url));
@@ -48,6 +55,23 @@ export async function proxy(request: NextRequest) {
 
   if (!accessToken && !isPublicRoute && !isAuthRoute) {
     return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  if (
+    pathname.startsWith("/customer-dashboard") &&
+    userRole !== Role.CUSTOMER
+  ) {
+    return NextResponse.redirect(new URL("/not-found", request.url));
+  } else if (
+    pathname.startsWith("/technician-dashboard") &&
+    userRole !== Role.TECHNICIAN
+  ) {
+    return NextResponse.redirect(new URL("/not-found", request.url));
+  } else if (
+    pathname.startsWith("/admin-dashboard") &&
+    userRole !== Role.ADMIN
+  ) {
+    return NextResponse.redirect(new URL("/not-found", request.url));
   }
 
   // return NextResponse.redirect(new URL("/", request.url));
